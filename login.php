6e0218e8835561d03b5cd6232fbbc82f0c0ffe14
pushkar makarand gadgil <pg395@afsconnect1.njit.edu>
2017-12-11 19:20:46 -0500

    added login file

--- login.php
+++ login.php
@@ -134,3 +134,22 @@ span.psw {
   
   <form action="/database.php">
   
+  
+  
+  <div class="container">
+      <label><b>Username</b></label><br>
+      <input type="text" placeholder="Enter Username" name="uname" required><br>
+
+      <label><b>Password</b></label><br>
+      <input type="password" placeholder="Enter Password" name="psw" required><br>
+        
+      <button type="submit">Login</button><br>
+      <input type="checkbox" checked="checked"> Remember me
+    </div>
+
+    <div class="container" style="background-color:#f1f1f1">
+      <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
+      <span class="psw">Forgot <a href="#">password?</a></span>
+    </div>
+  </form>
+</div>
