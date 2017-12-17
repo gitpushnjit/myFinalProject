3246477b8a834595def916095333413e6f665841
pushkar makarand gadgil <pg395@afsconnect2.njit.edu>
2017-12-17 15:57:31 -0500

    added all_accounts file into folder

---
+++ pages/all_accounts.php
@@ -0,0 +1,31 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>All Accounts</title>
+    <meta name="description" content="The HTML5 Herald">
+    <meta name="author" content="SitePoint">
+
+    <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+    <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+<h1>All records from Accounts is displayed here</h1>
+
+<h1> Hello Everyone..!!</h1>
+
+<?php
+//this is how you print something
+print htmlTable::genarateTableFromMultiArray($data);
+?>
+
+
+<script src="js/scripts.js"></script>
+</body>
+</html>
