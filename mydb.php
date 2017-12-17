95799ba2756c853571e1592d2164cc15ca72d828
pushkar makarand gadgil <pg395@afsconnect2.njit.edu>
2017-12-17 04:55:46 -0500

    updated mydb file

--- mydb.php
+++ mydb.php
@@ -1,16 +1,7 @@
- <?php
-$servername = "sql1.njit.edu";
-$username = "pg395";
-$password = "r4Matf7xW";
-
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=pg395", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
-    }
-catch(PDOException $e)
-    {
-    echo "Connection failed: " . $e->getMessage();
-    }
-?> 
+<?php
+namespace ConnectionNamespace;
+define('DATABASE', 'pg395');
+define('USERNAME', 'pg395');
+define('PASSWORD', 'r4Matf7xW');
+define('CONNECTION', 'sql.njit.edu');
+?>
