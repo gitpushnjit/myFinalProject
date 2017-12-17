12e12d20ec141baa820669ae67acbe63c92fa41d
pushkar makarand gadgil <pg395@afsconnect2.njit.edu>
2017-12-17 07:04:37 -0500

    updated controlTask file

--- controlTask.php
+++ controlTask.php
@@ -1,12 +1,6 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kwilliams
- * Date: 11/27/17
- * Time: 5:32 PM
- */
 //each page extends controller and the index.php?page=tasks causes the controller to be called
-class tasksController extends http\controller
+class controlTask extends corecontroller
 {
     //each method in the controller is named an action.
     //to call the show function the url is index.php?page=task&action=show
@@ -18,25 +12,35 @@ class tasksController extends http\controller
     //to call the show function the url is index.php?page=task&action=list_task
     public static function all()
     {
-        $records = todos::findAll();
-        /*session_start();
+        //$records = todos::findAll();
+        session_start();
            if(key_exists('userID',$_SESSION)) {
                $userID = $_SESSION['userID'];
            } else {
-               echo 'you must be logged in to view tasks';
+               header("Location: index.php?page=homepage&action=show");
            }
         $userID = $_SESSION['userID'];
         $records = todos::findTasksbyID($userID);
-        */
+        
         self::getTemplate('all_tasks', $records);
     }
     //to call the show function the url is called with a post to: index.php?page=task&action=create
     //this is a function to create new tasks
     //you should check the notes on the project posted in moodle for how to use active record here
     public static function create()
-    {
+    {   
+        session_start();
+        if(key_exists('userID',$_SESSION)) {
+            $userID = $_SESSION['userID'];
+        } else {
+            header("Location: index.php?page=homepage&action=show");
+        }
+        $userID = $_SESSION['userID'];
+        echo $userID;
         print_r($_POST);
+        self::getTemplate('edit_task');
     }
+    
     //this is the function to view edit record form
     public static function edit()
     {
@@ -46,10 +50,30 @@ class tasksController extends http\controller
     //this would be for the post for sending the task edit form
     public static function store()
     {
-        $record = todos::findOne($_REQUEST['id']);
-        $record->body = $_REQUEST['body'];
-        $record->save();
         print_r($_POST);
+        session_start();
+        if(key_exists('userID',$_SESSION)) {
+            $userID = $_SESSION['userID'];
+        } else {
+            header("Location: index.php?page=homepage&action=show");
+        }
+        $userID = $_SESSION['userID'];
+        if(isset($_REQUEST['id']) == 1){
+            $record = todos::findOne($_REQUEST['id']);
+        }
+        else {
+            $record = new todo();
+        }
+        $record->message = $_POST['message'];
+        $record->isdone = $_POST['isdone'];
+        $record->createddate = $_POST['createddate'];
+        $record->duedate = $_POST['duedate'];
+        $record->ownerid = $userID;
+        $record->owneremail = accounts::getEmail($userID);
+        echo $record->owneremail;
+        echo $record->createddate;
+        $record->save();
+        header("Location: index.php?page=tasks&action=all");
     }
     public static function save() {
         session_start();
@@ -65,5 +89,6 @@ class tasksController extends http\controller
         $record = todos::findOne($_REQUEST['id']);
         $record->delete();
         print_r($_POST);
+        header("Location: index.php?page=tasks&action=all");
     }
 }
