c0c72a85532a2de0956bce2273505d224fb53eb6
pushkar makarand gadgil <pg395@afsconnect2.njit.edu>
2017-12-17 16:07:22 -0500

    updated edit_account file

---
+++ pages/edit_account.php
@@ -0,0 +1,44 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>The HTML5 Herald</title>
+    <meta name="description" content="The HTML5 Herald">
+    <meta name="author" content="SitePoint">
+
+    <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+    <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+<?php
+//this is how you print something  $data contains the record that was selected on the table.
+print_r($data);
+?>
+
+<form action="index.php?page=accounts&action=edit&id=<?php echo $data->id; ?>" method="post">
+
+    First name: <input type="text" name="fname" value="<?php echo $data->fname; ?>"><br>
+
+    Last name: <input type="text" name="lname" value="<?php echo $data->lname; ?>"><br>
+    Email: <input type="text" name="email" value="<?php echo $data->email; ?>"><br>
+    Password: <input type="password" name="password" value="<?php echo $data->password; ?>" pattern=".{6,}" title="Password must be at least 6 characters" required><br>
+    <input type="submit" value="Submit form">
+</form>
+
+<h6><a href="index.php?page=tasks&action=all">Back</a></h6>
+
+<!-- <form action="index.php?page=accounts&action=delete&id=<?php echo $data->id; ?> " method="post" id="form1">
+    <button type="submit" form="form1" value="delete">Delete</button>
+</form> -->
+
+
+<script src="js/scripts.js"></script>
+</body>
+</html>
